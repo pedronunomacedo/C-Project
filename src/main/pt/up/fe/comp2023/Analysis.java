ab8973185eaf36632e9e32e44867a10a2bf47751
gonzallito <goncaloferreira2408@gmail.com>
2023-04-26 10:05:00 +0100

    20/20

--- src/main/pt/up/fe/comp2023/Analysis.java
+++ src/main/pt/up/fe/comp2023/Analysis.java
@@ -7,20 +7,33 @@ import pt.up.fe.comp.jmm.ast.JmmNode;
 import pt.up.fe.comp.jmm.report.Report;
 import pt.up.fe.comp.jmm.report.ReportType;
 import pt.up.fe.comp.jmm.report.Stage;
+import pt.up.fe.comp2023.ast.JmmMethod;
 import pt.up.fe.comp2023.ast.JmmSymbolTable;
 
 public class Analysis {
     private List<Report> reports;
     private JmmSymbolTable symbolTable;
 
+    private JmmMethod currMethod;
+
     Analysis(List<Report> reports, JmmSymbolTable symbolTable) {
         this.reports = reports;
         this.symbolTable = symbolTable;
+        this.currMethod = null;
     }
 
     Analysis(JmmSymbolTable symbolTable) {
         this.reports = new ArrayList<Report>();
         this.symbolTable = symbolTable;
+        this.currMethod = null;
+    }
+
+    public JmmMethod getCurrMethod() {
+        return currMethod;
+    }
+
+    public void setCurrMethod(JmmMethod currMethod) {
+        this.currMethod = currMethod;
     }
 
     public List<Report> getReports() {
