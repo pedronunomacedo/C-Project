1c744676e26c3f6d086c8d0e6fb77ecad6fe19fb
GoncaloDomingues750 <up202007914@up.pt>
2023-04-27 17:08:37 +0100

    Improved code organization for checkpoint 3

--- src/main/pt/up/fe/comp2023/Jasmin/JasminTypesInst.java
+++ src/main/pt/up/fe/comp2023/Jasmin/JasminTypesInst.java
@@ -5,6 +5,77 @@ import org.specs.comp.ollir.*;
 import java.util.ArrayList;
 
 public class JasminTypesInst {
+    public static String getType(Type t, ArrayList<String> imports, boolean methodParam) {
+        if (t.getTypeOfElement() == ElementType.INT32) {
+            return "I";
+        } else if (t.getTypeOfElement() == ElementType.BOOLEAN) {
+            return "Z";
+        } else if (t.getTypeOfElement() == ElementType.VOID) {
+            return "V";
+        } else if (t.getTypeOfElement() == ElementType.STRING) {
+            return "Ljava/lang/String;";
+        } else if (t.getTypeOfElement() == ElementType.OBJECTREF) {
+            return getObjectType((ClassType) t, imports, methodParam);
+        } else if (t.getTypeOfElement() == ElementType.ARRAYREF) {
+            return getArrayType((ArrayType) t, imports, methodParam);
+        } else {
+            return "";
+        }
+    }
+
+    public static String getInstructionType(Instruction instr, Method m) {
+        String result = "";
+        if (instr.getInstType() == InstructionType.ASSIGN) {
+            StringBuilder jasminCode = new StringBuilder();
+            AssignInstruction inst = (AssignInstruction) instr;
+            Element lhand = inst.getDest();
+            Instruction rhand = inst.getRhs();
+            jasminCode.append(JasminTypesInst.getInstructionType(rhand, m));
+            jasminCode.append("\t").append(Instructions.storeElem(lhand, m.getVarTable()));
+            result = jasminCode.toString();
+        }  else if (instr.getInstType() == InstructionType.GOTO) {
+            StringBuilder jasminCode = new StringBuilder();
+            GotoInstruction inst = (GotoInstruction) instr;
+            jasminCode.append("\t").append("goto ").append(inst.getLabel()).append("\n");
+        } else if (instr.getInstType() == InstructionType.CALL) {
+            result = Instructions.callInst((CallInstruction) instr, m);
+        } else if (instr.getInstType() == InstructionType.BRANCH) {
+            result = "";
+        }  else if (instr.getInstType() == InstructionType.PUTFIELD) {
+            result = Instructions.putField((PutFieldInstruction) instr, m);
+        } else if (instr.getInstType() == InstructionType.GETFIELD) {
+            result = Instructions.getField((GetFieldInstruction) instr, m);
+        } else if (instr.getInstType() == InstructionType.UNARYOPER) {
+            StringBuilder jasminCode = new StringBuilder();
+            result = jasminCode.toString();
+        } else if (instr.getInstType() == InstructionType.RETURN) {
+            result = Instructions.returnInst((ReturnInstruction) instr, m);
+        } else if (instr.getInstType() == InstructionType.BINARYOPER) {
+            result = Instructions.binaryOp((BinaryOpInstruction) instr, m);
+        } else if (instr.getInstType() == InstructionType.NOPER) {
+            SingleOpInstruction ins = (SingleOpInstruction) instr;
+            StringBuilder jasminCode = new StringBuilder();
+            jasminCode.append(Instructions.loadInst(ins.getSingleOperand(), m.getVarTable()));
+            result = jasminCode.toString();
+        }
+        return result;
+    }
+
+    private static String getArrayType(ArrayType arrayType, ArrayList<String> importList, boolean methodParam) {
+        StringBuilder jasminCode = new StringBuilder();
+        int num = arrayType.getNumDimensions();
+
+        for (int i = 0; i < num; i++) {
+            jasminCode.append("[");
+        }
+
+
+        String elementType = getType(arrayType.getElementType(), importList, methodParam);
+        jasminCode.append(elementType);
+
+        return jasminCode.toString();
+    }
+
 
     private static String getObjectType(ClassType t, ArrayList<String> imp, boolean methodParam) {
         StringBuilder jasminCode = new StringBuilder();
@@ -51,77 +122,4 @@ public class JasminTypesInst {
 
         return jasminCode.toString();
     }
-
-
-    public static String getType(Type t, ArrayList<String> imports, boolean methodParam) {
-        if (t.getTypeOfElement() == ElementType.INT32) {
-            return "I";
-        } else if (t.getTypeOfElement() == ElementType.BOOLEAN) {
-            return "Z";
-        } else if (t.getTypeOfElement() == ElementType.VOID) {
-            return "V";
-        } else if (t.getTypeOfElement() == ElementType.STRING) {
-            return "Ljava/lang/String;";
-        } else if (t.getTypeOfElement() == ElementType.OBJECTREF) {
-            return getObjectType((ClassType) t, imports, methodParam);
-        } else if (t.getTypeOfElement() == ElementType.ARRAYREF) {
-            return getArrayType((ArrayType) t, imports, methodParam);
-        } else {
-            return "";
-        }
-    }
-
-    public static String getInstructionType(Instruction instr, Method m) {
-        String result = "";
-        if (instr.getInstType() == InstructionType.ASSIGN) {
-            StringBuilder jasminCode = new StringBuilder();
-            AssignInstruction inst = (AssignInstruction) instr;
-            Element lhand = inst.getDest();
-            Instruction rhand = inst.getRhs();
-            jasminCode.append(JasminTypesInst.getInstructionType(rhand, m));
-            jasminCode.append("\t").append(Instructions.storeElement(lhand, m.getVarTable()));
-            result = jasminCode.toString();
-        }  else if (instr.getInstType() == InstructionType.GOTO) {
-            StringBuilder jasminCode = new StringBuilder();
-            GotoInstruction inst = (GotoInstruction) instr;
-            jasminCode.append("\t").append("goto ").append(inst.getLabel()).append("\n");
-        } else if (instr.getInstType() == InstructionType.CALL) {
-            result = Instructions.callInstructions((CallInstruction) instr, m);
-        } else if (instr.getInstType() == InstructionType.BRANCH) {
-            result = "";
-        }  else if (instr.getInstType() == InstructionType.PUTFIELD) {
-            result = Instructions.putFieldInstructions((PutFieldInstruction) instr, m);
-        } else if (instr.getInstType() == InstructionType.GETFIELD) {
-            result = Instructions.getFieldInstructions((GetFieldInstruction) instr, m);
-        } else if (instr.getInstType() == InstructionType.UNARYOPER) {
-            StringBuilder jasminCode = new StringBuilder();
-            result = jasminCode.toString();
-        } else if (instr.getInstType() == InstructionType.RETURN) {
-            result = Instructions.returnInstructions((ReturnInstruction) instr, m);
-        } else if (instr.getInstType() == InstructionType.BINARYOPER) {
-            result = Instructions.binaryOpInstructions((BinaryOpInstruction) instr, m);
-        } else if (instr.getInstType() == InstructionType.NOPER) {
-            SingleOpInstruction ins = (SingleOpInstruction) instr;
-            StringBuilder jasminCode = new StringBuilder();
-            jasminCode.append(Instructions.loadInstruction(ins.getSingleOperand(), m.getVarTable()));
-            result = jasminCode.toString();
-        }
-        return result;
-    }
-
-    private static String getArrayType(ArrayType arrayType, ArrayList<String> importList, boolean methodParam) {
-        StringBuilder jasminCode = new StringBuilder();
-        int num = arrayType.getNumDimensions();
-
-        for (int i = 0; i < num; i++) {
-            jasminCode.append("[");
-        }
-
-
-        String elementType = getType(arrayType.getElementType(), importList, methodParam);
-        jasminCode.append(elementType);
-
-        return jasminCode.toString();
-    }
-
 }
