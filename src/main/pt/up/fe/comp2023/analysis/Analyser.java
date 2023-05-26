50d6d2c625b235b94a6c105e3413b4decb7628f3
pedronunomacedo <pedronunomacedo@hotmail.com>
2023-05-26 17:17:30 +0100

    Trying to solve the semantics private test section3_TypeVerification_While_If_Array_While_Cond_Not_Bool v3

--- src/main/pt/up/fe/comp2023/analysis/Analyser.java
+++ src/main/pt/up/fe/comp2023/analysis/Analyser.java
@@ -121,7 +121,9 @@ public class Analyser extends AJmmVisitor<String, Void> {
                 for (JmmNode stmtChild : node.getChildren()) {
                     visit(stmtChild, method);
                 }
-                visit(node.getJmmChild(0), method);
+                if (node.getNumChildren() != 0) {
+                    visit(node.getJmmChild(0), method);
+                }
                 break;
             case "Conditional":
                 Type expressionTypeIf = this.expressionVisitor.visit(node.getJmmChild(0));
